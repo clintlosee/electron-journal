3b2caebac1a0658b335d11bbe7799ff0b7d29f2d
Clint Losee <clint.losee@gmail.com>
2019-06-19 15:17:09 -0600

    Adding journal app electron files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,208 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Markdown from 'markdown-to-jsx';
+import AceEditor from 'react-ace';
+import styled from 'styled-components';
+import brace from 'brace';
+import 'brace/mode/markdown';
+import 'brace/theme/dracula';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const electron = window.require('electron');
+const settings = window.require('electron-settings');
+const { ipcRenderer } = electron;
+const fs = electron.remote.require('fs');
+
+class App extends Component {
+  state = {
+    loadedFile: '',
+    filesData: [],
+    activeIndex: 0,
+    directory: settings.get('directory') || null,
+  };
+
+  constructor() {
+    super();
+
+    // On Load
+    const directory = settings.get('directory');
+    if (directory) {
+      this.loadAndReadFiles(directory);
+    }
+
+    ipcRenderer.on('new-file', (event, fileContent) => {
+      this.setState({
+        loadedFile: fileContent,
+      });
+    });
+    ipcRenderer.on('new-dir', (event, dir) => {
+      this.setState({
+        directory: dir,
+      });
+      settings.set('directory', directory);
+      this.loadAndReadFiles(directory);
+    });
+  }
+
+  loadAndReadFiles = directory => {
+    fs.readdir(directory, (err, files) => {
+      const filteredFiles = files.filter(file => file.includes('.md'));
+      const filesData = filteredFiles.map(file => ({
+        path: `${directory}/${file}`,
+      }));
+
+      this.setState(
+        {
+          filesData,
+        },
+        () => this.loadFile(0)
+      );
+    });
+  };
+
+  changeFile = index => () => {
+    const { activeIndex } = this.state;
+    if (index !== activeIndex) {
+      this.saveFile();
+      this.loadFile(index);
+    }
+  };
+
+  loadFile = index => {
+    const { filesData } = this.state;
+    const content = fs.readFileSync(filesData[index].path).toString();
+
+    this.setState({
+      loadedFile: content,
+      activeIndex: index,
+    });
+  };
+
+  saveFile = () => {
+    const { activeIndex, loadedFile, filesData } = this.state;
+    fs.writeFile(filesData[activeIndex].path, loadedFile, err => {
+      if (err) {
+        return console.error(err);
+      }
+      console.log('saved');
+    });
+  };
+
+  render() {
+    const { loadedFile, directory, filesData } = this.state;
+    return (
+      <AppWrap>
+        <Header>Journal</Header>
+        {directory ? (
+          <Split>
+            <FilesWindow>
+              {filesData.map((file, index) => (
+                <button onClick={this.changeFile(index)}>{file.path}</button>
+              ))}
+            </FilesWindow>
+            <CodeWindow>
+              <AceEditor
+                mode="markdown"
+                theme="dracula"
+                onChange={newContent => {
+                  this.setState({
+                    loadedFile: newContent,
+                  });
+                }}
+                name="markdown_editor"
+                value={loadedFile}
+              />
+            </CodeWindow>
+            <RenderedWindow>
+              <Markdown>{loadedFile}</Markdown>
+            </RenderedWindow>
+          </Split>
+        ) : (
+          <LoadingMessage>
+            <h1>Press CmdORCtrl+O to open a directory</h1>
+          </LoadingMessage>
+        )}
+      </AppWrap>
+    );
+  }
 }
 
 export default App;
+
+const AppWrap = styled.div`
+  margin-top: 23px;
+`;
+
+const Header = styled.header`
+  background-color: #191324;
+  color: #75717c;
+  font-size: 0.8rem;
+  height: 23px;
+  text-align: center;
+  position: fixed;
+  box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.2);
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 10;
+  -webkit-app-region: drag;
+`;
+
+const LoadingMessage = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  color: #fff;
+  background-color: #191324;
+`;
+
+const Split = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+
+const FilesWindow = styled.div`
+  background: #140f1d;
+  border-right: 1px solid #302b3a;
+  position: relative;
+  width: 20%;
+  &:after {
+    content: '';
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    pointer-events: none;
+    box-shadow: -10px 0 20px rgba(0, 0, 0, 0.3) inset;
+  }
+`;
+
+const CodeWindow = styled.div`
+  flex: 1;
+  padding-top: 2rem;
+  background-color: #191324;
+`;
+
+const RenderedWindow = styled.div`
+  background-color: #191324;
+  width: 35%;
+  padding: 20px;
+  color: #fff;
+  border-left: 1px solid #302b3a;
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    color: #82d8d8;
+  }
+  h1 {
+    border-bottom: solid 3px #e54b4b;
+    padding-bottom: 10px;
+  }
+  a {
+    color: #e54b4b;
+  }
+`;
---
+++ src/main.js
@@ -0,0 +1,215 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, dialog, Menu, isMac } = require('electron');
+const path = require('path');
+const fs = require('fs');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    titleBarStyle: 'hidden',
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  // and load the index.html of the app.
+  // mainWindow.loadFile('index.html');
+  mainWindow.loadURL('http://localhost:3000');
+
+  const template = [
+    // { role: 'appMenu' }
+    ...(process.platform === 'darwin'
+      ? [
+          {
+            label: app.getName(),
+            submenu: [
+              { role: 'about' },
+              { type: 'separator' },
+              { role: 'services' },
+              { type: 'separator' },
+              { role: 'hide' },
+              { role: 'hideothers' },
+              { role: 'unhide' },
+              { type: 'separator' },
+              { role: 'quit' },
+            ],
+          },
+        ]
+      : []),
+    // { role: 'fileMenu' }
+    {
+      label: 'File',
+      // submenu: [isMac ? { role: 'close' } : { role: 'quit' }],
+      submenu: [
+        {
+          label: 'Open Folder',
+          accelerator: 'CmdOrCtrl+O',
+          click() {
+            openDir();
+          },
+        },
+        { type: 'separator' },
+        {
+          ...(isMac ? { role: 'close' } : { role: 'quit' }),
+        },
+      ],
+    },
+    // { role: 'editMenu' }
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        ...(isMac
+          ? [
+              { role: 'pasteAndMatchStyle' },
+              { role: 'delete' },
+              { role: 'selectAll' },
+              { type: 'separator' },
+              {
+                label: 'Speech',
+                submenu: [{ role: 'startspeaking' }, { role: 'stopspeaking' }],
+              },
+            ]
+          : [{ role: 'delete' }, { type: 'separator' }, { role: 'selectAll' }]),
+      ],
+    },
+    // { role: 'viewMenu' }
+    {
+      label: 'View',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forcereload' },
+        { role: 'toggledevtools' },
+        { type: 'separator' },
+        { role: 'resetzoom' },
+        { role: 'zoomin' },
+        { role: 'zoomout' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' },
+      ],
+    },
+    // { role: 'windowMenu' }
+    {
+      label: 'Window',
+      submenu: [
+        { role: 'minimize' },
+        { role: 'zoom' },
+        ...(isMac
+          ? [
+              { type: 'separator' },
+              { role: 'front' },
+              { type: 'separator' },
+              { role: 'window' },
+            ]
+          : [{ role: 'close' }]),
+      ],
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click() {
+            require('electron').shell.openExternalSync(
+              'https://electronjs.org'
+            );
+          },
+        },
+      ],
+    },
+    {
+      label: 'Developer',
+      submenu: [
+        {
+          label: 'Toggle Dev Tools',
+          accelerator:
+            process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+          click() {
+            mainWindow.webContents.toggleDevTools();
+          },
+        },
+      ],
+    },
+  ];
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function() {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) createWindow();
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
+
+// Open File
+function openFile() {
+  const files = dialog.showOpenDialog(mainWindow, {
+    properties: ['openFile'],
+    filters: [
+      {
+        name: 'Markdown',
+        extensions: ['md', 'markdown', 'txt'],
+      },
+    ],
+  });
+
+  //* if no files
+  if (!files) return;
+
+  const file = files[0];
+  const fileContent = fs.readFileSync(file).toString();
+
+  //* Send fileContent to renderer
+  mainWindow.webContents.send('new-file', fileContent);
+}
+
+// Opens Directory
+function openDir() {
+  const directory = dialog.showOpenDialog(mainWindow, {
+    properties: ['openDirectory'],
+  });
+
+  //* if no directory
+  if (!directory) return;
+
+  const dir = directory[0];
+  mainWindow.webContents.send('new-dir', dir);
+}
